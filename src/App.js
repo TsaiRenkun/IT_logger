fa210a51345b5e5ceb00d5f41a0189aaa41cf3f1
Tsairenkun <Tsairenkun@gmail.com>
2020-07-06 20:48:35 +0800

    Setting up materialize-css & material Design Icon

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
-import React from 'react';
+import React, { useEffect } from "react";
+import "materialize-css/dist/css/materialize.min.css";
+import M from "materialize-css/dist/js/materialize.min.js";
 
 const App = () => {
-  return (
-    <div className="App">
-      HELLO REACT
-    </div>
-  );
-}
+  useEffect(() => {
+    //init Materialize Js
+    M.AutoInit();
+  });
+
+  return <div className="App">HELLO REACT</div>;
+};
 
 export default App;
