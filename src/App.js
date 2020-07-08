732e35b94dca493b3ff2d947da64101a0195875d
Tsairenkun <Tsairenkun@gmail.com>
2020-07-08 16:14:01 +0800

    Redux set up

--- src/App.js
+++ src/App.js
@@ -7,6 +7,10 @@ import EditLogModal from "./components/logs/EditLogModal";
 import AddTechModal from "./components/techs/AddTechModal";
 import TechListModal from "./components/techs/TechListModal";
 
+//redux
+import { Provider } from "react-redux";
+import store from "./store";
+
 import "materialize-css/dist/css/materialize.min.css";
 import M from "materialize-css/dist/js/materialize.min.js";
 import "./App.css";
@@ -18,17 +22,19 @@ const App = () => {
   });
 
   return (
-    <Fragment>
-      <SearchBar />
-      <div className="container">
-        <AddBtn />
-        <AddLogModal />
-        <EditLogModal />
-        <AddTechModal/>
-        <TechListModal/>
-        <Logs />
-      </div>
-    </Fragment>
+    <Provider>
+      <Fragment>
+        <SearchBar />
+        <div className="container">
+          <AddBtn />
+          <AddLogModal />
+          <EditLogModal />
+          <AddTechModal />
+          <TechListModal />
+          <Logs />
+        </div>
+      </Fragment>
+    </Provider>
   );
 };
 
